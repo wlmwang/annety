f72c4f1d59141ef5fbacbba17558d02ef2c44244
wlmwang <245432668@qq.com>
2019-08-19 23:56:13 +0800

    macos connect faild

--- src/Connector.cc
+++ src/Connector.cc
@@ -181,18 +181,120 @@ void Connector::connecting()
 
 	// When the socket becomes writable, the connection is successfully established
 	// But if the socket becomes readable or readable & writable, the connection fails
-	// ON LINUX, connect() failure, I/O select event is [IN OUT HUP ERR]
+	// ON LINUX, connect() failure, the multiplexer trigger event is [IN OUT HUP ERR]
+	// ON MACOS, connect() failure, the multiplexer trigger event is [IN PRI HUP]
 	using containers::make_weak_bind;
-	connect_channel_->set_write_callback(
-		make_weak_bind(&Connector::handle_write, shared_from_this()));
 	connect_channel_->set_error_callback(
 		make_weak_bind(&Connector::handle_error, shared_from_this()));
-	
-	// connect_channel_->set_read_callback(
-	// 	make_weak_bind(&Connector::handle_read, shared_from_this()));
-	// connect_channel_->enable_read_event();
+	connect_channel_->set_write_callback(
+		make_weak_bind(&Connector::handle_write, shared_from_this()));
+	connect_channel_->set_read_callback(
+		make_weak_bind(&Connector::handle_read, shared_from_this()));
 
+	connect_channel_->enable_read_event();
 	connect_channel_->enable_write_event();
+}
+
+void Connector::handle_write()
+{
+	owner_loop_->check_in_own_loop();
+
+	LOG(TRACE) << "Connector::handle_write the state=" << state_;
+
+	// When the socket becomes writable, the connection is successfully established
+	// But if the socket becomes readable or readable & writable, the connection fails
+	// ON LINUX, connect() failure, the multiplexer trigger event is [IN OUT HUP ERR]
+	// ON MACOS, connect() failure, the multiplexer trigger event is [IN PRI HUP]
+	if (state_ == kConnecting) {
+		DCHECK(connect_socket_);
+		remove_and_reset_channel();
+
+		int err = internal::get_sock_error(*connect_socket_);
+		if (err) {
+			ScopedClearLastError last_error;
+			errno = err;
+			PLOG(WARNING) << "Connector::handle_write connect has failed";
+
+			connect_socket_.reset();
+			state_ = kDisconnected;
+
+			// connect failure
+			if (error_connect_cb_) {
+				error_connect_cb_();
+			}
+		} else {
+			state_ = kConnected;
+			if (connect_) {
+				if (new_connect_cb_) {
+					new_connect_cb_(std::move(connect_socket_), server_addr_);
+				} else {
+					// discard connection
+					connect_socket_.reset();
+				}
+			} else {
+				// discard connection
+				connect_socket_.reset();
+			}
+		}
+	}
+}
+
+void Connector::handle_read()
+{
+	owner_loop_->check_in_own_loop();
+
+	LOG(WARNING) << "Connector::handle_read the state=" << state_;
+	
+	if (state_ == kConnecting) {
+		DCHECK(connect_socket_);
+		remove_and_reset_channel();
+
+		int err = internal::get_sock_error(*connect_socket_);
+		{
+			ScopedClearLastError last_error;
+			errno = err;
+			PLOG(WARNING) << "Connector::handle_read has failed";
+		}
+
+		connect_socket_.reset();
+		state_ = kDisconnected;
+
+		// connect failure
+		if (err) {
+			if (error_connect_cb_) {
+				error_connect_cb_();
+			}
+		}
+	}
+}
+
+void Connector::handle_error()
+{
+	owner_loop_->check_in_own_loop();
+
+	LOG(WARNING) << "Connector::handle_error the state=" << state_;
+	
+	if (state_ == kConnecting) {
+		DCHECK(connect_socket_);
+		remove_and_reset_channel();
+
+		int err = internal::get_sock_error(*connect_socket_);
+		{
+			ScopedClearLastError last_error;
+			errno = err;
+			PLOG(WARNING) << "Connector::handle_error has failed";
+		}
+		
+		connect_socket_.reset();
+		state_ = kDisconnected;
+
+		// connect failure
+		if (err) {
+			if (error_connect_cb_) {
+				error_connect_cb_();
+			}
+		}
+	}
 }
 
 void Connector::retry_in_own_loop()
@@ -219,103 +321,6 @@ void Connector::retry_in_own_loop()
 	}
 }
 
-void Connector::handle_write()
-{
-	owner_loop_->check_in_own_loop();
-	
-	ScopedClearLastError last_error;
-
-	LOG(TRACE) << "Connector::handle_write the state=" << state_;
-
-	// When the socket becomes writable, the connection is successfully established
-	// But if the socket becomes readable or readable & writable, the connection fails
-	if (state_ == kConnecting) {
-		DCHECK(connect_socket_);
-		remove_and_reset_channel();
-
-		int err = internal::get_sock_error(*connect_socket_);
-		if (err) {
-			ScopedClearLastError last_error;
-			errno = err;
-			PLOG(WARNING) << "Connector::handle_write connect has failed";
-
-			// retry_in_own_loop();	// FIXME: is retry_in_own_loop() here???
-		} else if (internal::is_self_connect(*connect_socket_)) {
-			LOG(WARNING) << "Connector::handle_write connect self socket";
-			
-			// retry_in_own_loop();	// FIXME: is retry_in_own_loop() here???
-		} else {
-			state_ = kConnected;
-			if (connect_) {
-				if (new_connect_cb_) {
-					new_connect_cb_(std::move(connect_socket_), server_addr_);
-				} else {
-					// discard connection
-					connect_socket_.reset();
-				}
-			} else {
-				// discard connection
-				connect_socket_.reset();
-			}
-		}
-	} else {
-		// connect failure, the multiplexer will trigger event: POLLIN POLLOUT POLLHUP POLLERR
-		// so the connect channel will call handle_error, handle_read, handle_write sequentially!
-		DCHECK(state_ == kDisconnected);
-	}
-}
-
-void Connector::handle_read()
-{
-	owner_loop_->check_in_own_loop();
-
-	LOG(ERROR) << "Connector::handle_read the state=" << state_;
-	
-	if (state_ == kConnecting) {
-		DCHECK(connect_socket_);
-		remove_and_reset_channel();
-
-		int err = internal::get_sock_error(*connect_socket_);
-		{
-			ScopedClearLastError last_error;
-			errno = err;
-			PLOG(DEBUG) << "Connector::handle_read has failed";
-		}
-
-		connect_socket_.reset();
-		state_ = kDisconnected;
-	}
-}
-
-void Connector::handle_error()
-{
-	owner_loop_->check_in_own_loop();
-
-	LOG(ERROR) << "Connector::handle_error the state=" << state_;
-	
-	if (state_ == kConnecting) {
-		DCHECK(connect_socket_);
-		remove_and_reset_channel();
-
-		int err = internal::get_sock_error(*connect_socket_);
-		{
-			ScopedClearLastError last_error;
-			errno = err;
-			PLOG(ERROR) << "Connector::handle_error has failed";
-		}
-		
-		connect_socket_.reset();
-		state_ = kDisconnected;
-
-		// connect failure
-		if (err) {
-			if (error_connect_cb_) {
-				error_connect_cb_();
-			}
-		}
-	}
-}
-
 void Connector::remove_and_reset_channel()
 {
 	owner_loop_->check_in_own_loop();
@@ -324,8 +329,6 @@ void Connector::remove_and_reset_channel()
 		connect_channel_->disable_all_event();
 		connect_channel_->remove();
 	}
-
-	LOG(ERROR) << "Connector::remove_and_reset_channel ###";
 
 	// can't reset Channel here, because we are inside Channel::handle_event
 	using containers::make_weak_bind;
@@ -336,16 +339,6 @@ void Connector::remove_and_reset_channel()
 void Connector::reset_channel()
 {
 	owner_loop_->check_in_own_loop();
-	
-	LOG(ERROR) << "Connector::remove_and_reset_channel @@@";
-
-	// FIXME: HACK FOR stop_in_own_loop()
-	// because the owner_loop_ could be delete, then queue_in_own_loop() is 
-	// do not work in remove_and_reset_channel()
-	if (connect_channel_ && !connect_channel_->is_none_event()) {
-		connect_channel_->disable_all_event();
-		connect_channel_->remove();
-	}
 
 	connect_channel_.reset();
 }
